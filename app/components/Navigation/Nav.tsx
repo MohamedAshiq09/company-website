1d3d2188fc24560476851487abba87b95dc1ea33
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-10-19 21:19:27 +0530

    updated NAVBAR

--- app/components/Navigation/Nav.tsx
+++ app/components/Navigation/Nav.tsx
@@ -4,20 +4,24 @@ import { Bars3BottomRightIcon } from "@heroicons/react/16/solid";
 import Link from "next/link";
 import React from "react";
 
-function Nav() {
+interface Props {
+  openNav: () => void;
+}
+
+function Nav({ openNav }: Props) {
   return (
-    <div className="h-[12vh] bg-gray-300 shadow-md w-full max-w-7xl mx-auto rounded-3xl mt-4 px-4">
+    <div className="h-[12vh] bg-gray-300 shadow-md w-full max-w-7xl mx-auto rounded-3xl mt-4 px-4 sm:px-6 lg:px-8">
       <div className="w-[85%] flex items-center h-[12vh] justify-between mx-auto ">
         <h1 className="text-[14px] md:text-[23px] font-bold text-slate-600">
-          <span className="text-[27px] md:[40px] text-red-600 ">A</span>
-          erospere
+          <span className="text-[27px] md:[40px] text-red-600 ">S</span>
+          anthosh
         </h1>
         <ul className="hidden lg:flex items-center  space-x-10">
           <li className="text-[18px] curser-pointer text-bold text-red-400">
             <Link href={"/"}>Home</Link>
           </li>
           <li className="text-[18px] curser-pointer text-bold hover:text-red-400 transition-all duration-200">
-            <Link href={"/"}>About</Link>
+            <Link href={"/about"}>About</Link>
           </li>
           <li className="text-[18px] curser-pointer text-bold hover:text-red-400 transition-all duration-200">
             <Link href={"/"}>Services</Link>
@@ -33,7 +37,10 @@ function Nav() {
           </li>
         </ul>
         <div>
-          <Bars3BottomRightIcon className="w-[1.5rem] lg:hidden h-[1.5rem] text-slate-900 cursor-pointer" />
+          <Bars3BottomRightIcon
+            onClick={openNav}
+            className="w-[1.5rem] lg:hidden h-[1.5rem] text-slate-900 cursor-pointer"
+          />
         </div>
       </div>
     </div>
--- app/components/Navigation/ResponsiveNav.tsx
+++ app/components/Navigation/ResponsiveNav.tsx
@@ -1,13 +1,16 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
 
 function ResponsiveNav() {
+  const [showNav, setShowNav] = useState(false);
+  const openNavHandler = () => setShowNav(true);
+  const closeNavHanndler = () => setShowNav(false);
   return (
     <div>
-      <Nav />
-      <MobileNav />
+      <Nav openNav={openNavHandler} />
+      <MobileNav nav={showNav} closeNav={closeNavHanndler} />
     </div>
   );
 }
