6324ca369ef1e8ecc1a5e6d825017a272485584d
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-10-28 15:54:53 +0530

    added

---
+++ app/pages/who-we-are.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const WhoWeAre = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Who We Are</h1>
+      <p>Learn more about who we are...</p>
+    </div>
+  );
+};
+
+export default WhoWeAre;
