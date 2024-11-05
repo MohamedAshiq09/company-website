bd780434b6c903dff099002326f23d65c4983a4b
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-05 21:38:28 +0530

    added

--- app/components/three/Model.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useGLTF } from '@react-three/drei';
-
-const Model = () => {
-  const { scene } = useGLTF('/model.gltf'); 
-  return <primitive object={scene} scale={0.5} />;
-};
-
-export default Model;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+"use client"; 
 import Home1 from "./home/page";
+
 
 export default function Home() {
   return (
     <div className="bg--900 min-h-screen">
       <Home1 />
+      <div style={{ height: '100vh' }}>
+       
+      </div>
     </div>
   );
 }
