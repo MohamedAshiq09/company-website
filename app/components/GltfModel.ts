7b81344f292fd4b16afca46d09c661ebd369ee73
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-11-10 17:31:08 +0530

    added

---
+++ app/components/GltfModel.ts
@@ -0,0 +1,17 @@
+// // components/GltfModel.js
+// import { Canvas } from '@react-three/fiber';
+// import { useGLTF } from '@react-three/drei';
+
+// const GltfModel = () => {
+//   const model = useGLTF('/path/to/your/model.gltf'); // Replace with the path to your GLTF file
+
+//   return (
+//     <Canvas>
+//       <ambientLight intensity={0.5} />
+//       <directionalLight position={[10, 10, 5]} intensity={1} />
+//       <primitive object={model.scene} scale={1} />
+//     </Canvas>
+//   );
+// };
+
+// export default GltfModel;
